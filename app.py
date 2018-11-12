a962e2bcc9eb61ee186c49313fc0c95531981cfd
Josh Kearney <jpk518@mun.ca>
2018-11-11 20:09:53 -0500

    commit all

--- app.py
+++ app.py
@@ -2,54 +2,23 @@ from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
+
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-workplaces = [
-    {
-        'Title': 'General Dynamics',
-        'About': 'Military',
-        'Accomplishments': 'Networking'
-    },
-    {
-        'Title': 'DuXion Motors',
-        'About': 'Aerospace Startup',
-        'Accomplishments': 'Thermal Systems'
-    },
-    {
-        'Title': 'Rheinmetall Landsysteme',
-        'About': 'Military',
-        'Accomplishments': 'Networking'
-    },
-    {
-        'Title': 'Bell Canada',
-        'About': 'Radio/Telephone Company',
-        'Accomplishments': 'Networking'
-    }
-]
 
-projects = [
-    {
-        'Title': 'Kraken Robotics',
-        'About': 'Robotics',
-        'Accomplishments': 'Software'
-    },
-    {
-        'Title': 'Paradigm Hyperloop',
-        'About': 'Robotics',
-        'Accomplishments': 'Software'
-    },
-    {
-        'Title': 'Personal Projects',
-        'About': 'Robotics',
-        'Accomplishments': 'Software'
-    },
-]
-
+with open("json/workplaces.json","r") as workFile:
+    workplaces = json.load(workFile)
+with open("json/projects.json", "r") as projectFile:
+    projects = json.load(projectFile)
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', workplaces=workplaces, projects=projects)
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == 'main':
     app.run(debug=True)
